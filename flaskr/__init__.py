2a0fe71f5fd889c4fb3f64e6e130bc8b7bbcc885
Xunjie <xunjie.liu16@student.xjtlu.edu.cn>
2019-01-21 04:22:39 +0800

    define and access the database

---
+++ flaskr/__init__.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    ) # 看这格式，json文件？
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        # 如果是测试样例，就从config文件加载配置文件
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    return app
+
